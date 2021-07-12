07d9c4aa7ef8ef62e3955a2d52ddc0320c56d6cd
Chrisando Ryan <chrisando.siahaan@gmail.com>
2021-07-12 15:33:56 +0700

    first commit

---
+++ ocr_engine/dataset/helpers.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+import cv2
+
+def load_dataset(datasetPath):
+    images = []
+    labels = []
+    for root, dirs, files in os.walk(datasetPath):
+        label = os.path.basename(os.path.normpath(root))
+        for fname in files:
+            if fname.startswith('.') and os.path.isfile(os.path.join(root, fname)):
+                continue
+            image = cv2.imread(os.path.join(root, fname))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            images.append(image)
+            labels.append(label)
+    return images, labels
+
+
---
+++ train.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from imutils import build_montages
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+from ocr_engine.models.resnet import ResNet
+from ocr_engine.dataset.helpers import load_dataset
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+# import the necessary packages
+
+print("[INFO] Loading datasets...")
+images, labels = load_dataset(
+	"../Datasets/DatasetGeneration/Calibri-MSWord-Images")
+assert len(images) == len(labels)
+
+# initialize the number of epochs to train for, initial learning rate,
+# and batch size
+EPOCHS = 50
+INIT_LR = 1e-1
+BS = 128
+
+# each image in the A-Z and MNIST digts datasets are 28x28 pixels;
+# however, the architecture we're using is designed for 32x32 images,
+# so we need to resize them to 32x32
+images = [cv2.resize(image, (32, 32)) for image in images]
+images = np.array(images, dtype="float32")
+
+# add a channel dimension to every image in the dataset and scale the
+# pixel intensities of the images from [0, 255] down to [0, 1]
+# images = np.expand_dims(images, axis=-1)
+# images /= 255.0
+
+# convert the labels from integers to vectors
+le = LabelBinarizer()
+labels = le.fit_transform(labels)
+counts = labels.sum(axis=0)
+
+# account for skew in the labeled data
+classTotals = labels.sum(axis=0)
+classWeight = {}
+
+# loop over all classes and calculate the class weight
+for i in range(0, len(classTotals)):
+	classWeight[i] = classTotals.max() / classTotals[i]
+
+# partition the data into training and testing splits using 80% of
+# the data for training and the remaining 20% for testing
+(trainX, testX, trainY, testY) = train_test_split(images,
+                                                  labels, test_size=0.20, stratify=labels, random_state=42)
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(
+	rotation_range=10,
+	zoom_range=0.05,
+	width_shift_range=0.1,
+	height_shift_range=0.1,
+	shear_range=0.15,
+	horizontal_flip=False,
+	fill_mode="nearest")
+
+# initialize and compile our deep neural network
+print("[INFO] Compiling model...")
+opt = SGD(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model = ResNet.build(32, 32, 1, len(le.classes_), (3, 3, 3),
+                     (64, 64, 128, 256), reg=0.0005)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the network
+print("[INFO] Training network...")
+H = model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	validation_data=(testX, testY),
+	steps_per_epoch=len(trainX) // BS,
+	epochs=EPOCHS,
+	class_weight=classWeight,
+	verbose=1)
+
+# define the list of label names
+labelNames = set(labels)
+
+# evaluate the network
+print("[INFO] Evaluating network...")
+predictions = model.predict(testX, batch_size=BS)
+print(classification_report(testY.argmax(axis=1),
+	predictions.argmax(axis=1), target_names=labelNames))
+
+# save the model to disk
+print("[INFO] serializing network...")
+model.save("camstroke-ocr.model", save_format="h5")
+
+# construct a plot that plots and saves the training history
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
